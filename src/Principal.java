84a640a33a0d908e5d4d9ec14ca54b8292d1031f
Txillu <qingyu.jiang@alumni.mondragon.edu>
2021-12-10 16:42:45 +0100

    44ghjk

--- src/Principal.java
+++ src/Principal.java
@@ -7,7 +7,7 @@ public class Principal {
 			System.out.println("holassss");
 			System.out.println("holaaaasswwssssssss");
 			System.out.println("txild4edelu");
-			System.out.println("todo tuyo");
+			System.out.println("todos tubhthnyhjyo");
 	}
 
 }
