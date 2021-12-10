282cf3f84a320ed98df88698b533741f59a14e39
inakiAramburu <inaki.aramburur@alumni.mondragon.edu>
2021-12-10 16:09:52 +0100

    asas

--- src/Principal.java
+++ src/Principal.java
@@ -5,7 +5,7 @@ public class Principal {
 		// TODO Auto-generated method stub
 
 			System.out.println("holassss");
-			System.out.println("holaaaasssssss");
+			System.out.println("holaaaassssssssss");
 	}
 
 }
