7c0987aca5fd0803705cb0fe86ac215580d925b0
inakiAramburu <inaki.aramburur@alumni.mondragon.edu>
2022-01-10 13:45:37 +0100

    errorr de mierda arreglado

--- src/Principal.java
+++ src/Principal.java
@@ -1,26 +1,36 @@
-import com.fazecast.jSerialComm.*;
+import jssc.SerialPortList;
+
+//https://code.google.com/archive/p/java-simple-serial-connector/wikis/jSSC_examples.wiki
+
+
 
 public class Principal {
-	SerialPort[] puertos;
+
 	public Principal() {
-		puertos = SerialPort.getCommPorts();
+
 	}
-	
-//	@Override
-//	public int getListeningEvents() {
-//		// TODO Auto-generated method stub
-//		return 0;
-//	}
-//
-//	@Override
-//	public void serialEvent(SerialPortEvent arg0) {
-//		// TODO Auto-generated method stub
-//
-//	}
+
+	// @Override
+	// public int getListeningEvents() {
+	// // TODO Auto-generated method stub
+	// return 0;
+	// }
+	//
+	// @Override
+	// public void serialEvent(SerialPortEvent arg0) {
+	// // TODO Auto-generated method stub
+	//
+	// }
 
 	public static void main(String[] args) {
 		Principal programa = new Principal();
+		System.out.println("hola");
+		
+		// mostrar una lista de los puertos disponibles en consola
+		String[] portNames = SerialPortList.getPortNames();
+		for (int i = 0; i < portNames.length; i++) {
+			System.out.println(portNames[i]);
+		}
 
 	}
-
 }
